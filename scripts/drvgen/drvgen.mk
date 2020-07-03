b76869b29c2e07f9afa0b3400ea5c07a63c8386b
parthibx24 <e.inxpired@gmail.com>
2020-07-03 16:32:08 +0600

    drvgen: force: make use of old-drvgen
    It'll use the old version of drvgen which is enough to manipulate dws of
    our projects [k80]. It also makes the source buildable without python2.
    
    Signed-off-by: parthibx24 <e.inxpired@gmail.com>
    Change-Id: I43187a47aa78ac5cc38b90a30923ccf35c085625

--- scripts/drvgen/drvgen.mk
+++ scripts/drvgen/drvgen.mk
@@ -54,6 +54,7 @@ else
 DRVGEN_FILE_LIST :=
 endif
 DRVGEN_TOOL := $(srctree)/tools/dct/DrvGen.py
+DRVGEN_TOOL_OLD := $(srctree)/tools/dct/old_dct/DrvGen
 DRVGEN_FIG := $(wildcard $(dir $(DRVGEN_TOOL))config/*.fig)
 
 .PHONY: drvgen
@@ -65,8 +66,8 @@ $(DRVGEN_FILE_LIST): $(DRVGEN_TOOL) $(DWS_FILE) $(DRVGEN_FIG) $(PROJ_DTS_FILES)
 		prj_path=$(DRVGEN_OUT)/$$base_prj ;\
 		dws_path=$(srctree)/$(DRVGEN_PATH)/$$base_prj.dws ;\
 		if [ -f $$dws_path ] ; then \
-			mkdir -p $$prj_path ;\
-			$(python) $(DRVGEN_TOOL) $$dws_path $$prj_path $$prj_path cust_dtsi;\
+			mkdir -p $$prj_path;\
+			($(DRVGEN_TOOL_OLD) $$dws_path $$prj_path $$prj_path cust_dtsi); \
 		fi \
 	done
 
